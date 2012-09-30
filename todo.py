33627627f5c2c6949d6a71f992eb23d020d64cf0
Alex Gagnon <gagnon.alex@gmail.com>
2012-09-30 10:27:16 -0700

    Updated todo.py

--- todo.py
+++ todo.py
@@ -1,3 +1,7 @@
 # Concept inventory for reference card system
 
-# Concepts: card, create, delete, add_to, project, priority, priorityList, task, TaskGroup, taskDict, items, context, pending[], getFile, fetch, source, class Event(object), class CollectionStartEvent(NodeEvent), node, edge, anchor, token/tag, refcard, log_file. See: Gitmarks
+# Concepts: card, create, delete, add_to, project,
+# priority, priorityList, task, TaskGroup, taskDict,
+# items, context, pending[], getFile, fetch, source,
+# class Event(object), class CollectionStartEvent(NodeEvent),
+# node, edge, anchor, token/tag, refcard, log_file. See: Gitmarks
