4318a88663a92c491e432d4f0a90564b0e3d5617
Alex Gagnon <gagnon.alex@gmail.com>
2012-12-17 02:06:08 -0800

    Updated todo.py

--- todo.py
+++ todo.py
@@ -1,5 +1,10 @@
 # Concept inventory for reference card system
 
+# High Level
+# card, cardBox, cardReferences, cardType, cardElements, referenceDict, cardCollection,
+# collectionID, referenceID
+
+# Low Level
 # Concepts: card, create, delete, add_to, project,
 # priority, priorityList, task, TaskGroup, taskDict,
 # items, context, pending[], getFile, fetch, source,
