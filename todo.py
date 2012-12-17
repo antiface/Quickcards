7b720eaf063f72553560afeaec13e1a7ca85f1ee
Alex Gagnon <gagnon.alex@gmail.com>
2012-12-17 01:54:28 -0800

    Updated todo.py

--- todo.py
+++ todo.py
@@ -5,3 +5,15 @@
 # items, context, pending[], getFile, fetch, source,
 # class Event(object), class CollectionStartEvent(NodeEvent),
 # node, edge, anchor, token/tag, refcard, log_file. See: Gitmarks
+#
+# Classes/Entities
+# card/node, cardID, collectionID, cardReferences, cardElements
+#
+#
+#
+#
+#
+#
+#
+#
+#
