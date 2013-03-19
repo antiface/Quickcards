99a7e68ebf843e006a9ee4c6f0b59e246c453066
Alex Gagnon <gagnon.alex@gmail.com>
2013-03-19 16:32:58 -0700

    Updated card.py

--- card.py
+++ card.py
@@ -1,1 +1,25 @@
 class Card(object):
+
+def __init__(self,
+               created_at=None,
+               id=None,
+               text=None,
+               user=None,
+               source=None,
+               urls=None,
+               hashtags=None,
+               place=None,
+               contributors=None,
+               
+    """An object to hold card contents.
+    
+    self.created_at = created_at
+    self.id = id
+    self.text = text
+    self.user = user
+    self.source = source
+    self.urls = urls
+    self.hashtags = hashtags
+    self.place = place
+    self.contributors = contributors
+    """
