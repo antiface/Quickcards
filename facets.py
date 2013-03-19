19feafe1a209b1729ccd80070ba8cd53ac6d8a7e
Alex Gagnon <gagnon.alex@gmail.com>
2013-03-19 16:46:03 -0700

    Updated facets.py

--- facets.py
+++ facets.py
@@ -1,2 +1,21 @@
-class Facets(object):
+class Facets(dict):
 
+def __init__(self, slug, name, facets=[], description=""):
+        self.name = name
+        self.slug = slug
+        self._facet_lookup = {}
+        self.description = description
+
+""" Snippet for reference for json objects later on.
+{
+        "pk": 1,
+        "model": "tests.user",
+        "fields": {
+            "username": "alex",
+            "status": 1,
+            "first_name": "",
+            "last_name": "",
+            "favorite_books": [1, 2],
+            "is_active": false
+        }
+"""
