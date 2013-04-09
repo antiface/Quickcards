d12eeaafd2932b596f2bbc0df42c3f1192cb2149
Alex Gagnon <gagnon.alex@gmail.com>
2013-04-09 00:39:21 -0300

    Update card.py

--- card.py
+++ card.py
@@ -24,3 +24,63 @@ def __init__(self,
     self.place = place
     self.contributors = contributors
     """
+    
+  def GetCreatedAt(self):
+    '''Get the time this card was created.
+
+    Returns:
+      The time this card was created.
+    '''
+    return self._created_at
+
+  def SetCreatedAt(self, created_at):
+    '''Set the time this card was created.
+
+    Args:
+      created_at:
+        The time this card was created
+    '''
+    self._created_at = created_at
+
+  created_at = property(GetCreatedAt, SetCreatedAt,
+                        doc='The time this card was created.')
+
+  def GetId(self):
+    '''Get the unique id of this card.
+
+    Returns:
+      The unique id of this card.
+    '''
+    return self._id
+
+  def SetId(self, id):
+    '''Set the unique id of this card.
+
+    Args:
+      id:
+        The unique id of this card.
+    '''
+    self._id = id
+
+  id = property(GetId, SetId,
+                doc='The unique id of this card.')
+                
+  def GetText(self):
+    '''Get the text of card.
+
+    Returns:
+      The text of this card.
+    '''
+    return self._text
+
+  def SetText(self, text):
+    '''Set the text of this card.
+
+    Args:
+      text:
+        The text of this card.
+    '''
+    self._text = text
+
+  text = property(GetText, SetText,
+                  doc='The text of this card')
