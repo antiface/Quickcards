c34c47d4a1635dbd3aa64be601aaa8c800e3865c
Alex Gagnon <gagnon.alex@gmail.com>
2013-06-27 19:49:43 -0400

    Update currentForecast.py

--- Cards/moraltempcards/currentForecast.py
+++ Cards/moraltempcards/currentForecast.py
@@ -1,7 +1,7 @@
 import urllib
 import simplejson
  
-# Replace /APIKEY/LATITUDE,LONGITUDE with appropriate information.
+# Replace /APIKEY/LATITUDE,LONGITUDE with appropriate information + add query parameter ?unit=ca to get units in Canadian format.
  
 request = urllib.urlopen('https://api.forecast.io/forecast/APIKEY/LATITUDE,LONGITUDE')
 forecastData = simplejson.loads(request.read())
