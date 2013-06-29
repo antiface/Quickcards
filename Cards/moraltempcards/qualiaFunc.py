704229203c1a8f6e4c47bc94a9c564f92631feaa
Alex Gagnon <gagnon.alex@gmail.com>
2013-06-28 20:47:43 -0400

    Create qualiaFunc.py

---
+++ Cards/moraltempcards/qualiaFunc.py
@@ -0,0 +1,17 @@
+import time
+qualia = []
+adict = {}
+ 
+def qualiaFunc():
+  adict = {'situation':{'date': time.asctime(), 'place': raw_input("Where are you?: "), 'context': raw_input("What is the current Context?: ")}}
+	def setQualia():
+		for i in range(3):
+			qualia.append(raw_input("Qualify situation: "))
+		return qualia
+	def addQualiaToDict(adict):
+		x = setQualia()
+		adict.update({'qualia': x})
+	addQualiaToDict(adict)
+	return adict
+ 
+adict = qualiaFunc()
